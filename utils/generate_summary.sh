70b93d5ea7d0661ada7827dfbb73e44561ca0d09
h4ckd0tm3 <marcel@schnideritsch.at>
2023-04-27 13:16:02 +0200

    Update utility

--- utils/generate_summary.sh
+++ utils/generate_summary.sh
@@ -4,10 +4,14 @@ cd ../Curriculum
 echo "# Table of contents" > SUMMARY.md
 echo "* [Welcome](../README.md)" >> SUMMARY.md
 
-for d in */; do
-  if [ -f "$d/introduction.md" ]; then
-    echo "" >> SUMMARY.md
-    echo "### ${d%/}" >> SUMMARY.md
-    echo "* [Introduction](./$(echo "$d" | sed 's/ /%20/g')introduction.md)" >> SUMMARY.md
+find . -mindepth 1 -maxdepth 1 -type d -exec bash -c '
+  if [ -f "$1/introduction.md" ]; then
+    echo "$1 $(echo "$1" | tr -dc "0-9")"
   fi
+' _ {} \; | sort -k2n | while read d; do
+  d=${d% *}
+  echo "" >> SUMMARY.md
+  echo "### ${d##*/}" >> SUMMARY.md
+  echo "* [Introduction]($(echo "$d" | sed 's/ /%20/g')/introduction.md)" >> SUMMARY.md
 done
+
